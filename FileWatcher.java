a33075eb7702a5835902cc816befa6c5f933829d
zckkte <kite.antonio@gmail.com>
2017-04-01 11:29:56 +1300

    Use file last modified rather than file content digest; Add execute command

--- FileWatcher.java
+++ FileWatcher.java
@@ -1,56 +1,24 @@
 import java.util.*;
 import java.io.*;
-import java.security.*;
 
 public abstract class FileWatcher extends TimerTask {
 
-    private MessageDigest digest;
+    private long timeStamp;
     private File file;
 
     protected abstract void onChange(File file);
 
-    public FileWatcher(File file) throws IOException, NoSuchAlgorithmException {
+    public FileWatcher(File file) {
         this.file = file;
-        this.digest = this.computeFileDigest(file);
+        this.timeStamp = file.lastModified();
     }
 
     public final void run() {
-        try {
-            MessageDigest digest = this.computeFileDigest(this.file);
-            if (this.digest != digest) {
-                this.digest = digest;
-                System.out.println(digestToString(digest));
-                onChange(file);
-            }
-        } catch (IOException | NoSuchAlgorithmException ex) {
-            ex.printStackTrace();
-            return;
+        long timeStamp = file.lastModified();
+        if (timeStamp != this.timeStamp)
+        {
+            this.timeStamp = timeStamp;
+            onChange(file);
         }
-    }
-
-    protected MessageDigest computeFileDigest(File file) 
-        throws IOException, NoSuchAlgorithmException {
-
-        MessageDigest digest = null;
-        int nRead = 0;
-        byte[] buffer = new byte[1000];
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-          digest = MessageDigest.getInstance("MD5");
-          while ((nRead = inputStream.read(buffer)) != -1) {
-              digest.update(buffer, 0, nRead);
-          }
-        }
-
-        return digest;
-    }
-
-    private String digestToString(MessageDigest digest) {
-        byte[] mdbytes = digest.digest();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < mdbytes.length; i++) {
-            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-        }
-
-        return sb.toString();
     }
 }
--- WatchFile.java
+++ WatchFile.java
@@ -2,37 +2,60 @@ import java.util.*;
 import java.io.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.security.*;
 
-public class WatchDoProgram {
+public class WatchFile {
 
     public static int pollRate = 1000;
+    public static String DateFormat = "yyyy/MM/dd HH:mm:ss";
 
     public static void main(String[] args) {
         String fileName = args[0];
+        String command = args[1];
 
         File file = new File(fileName);
         if (!file.exists() || file.isDirectory()) {
-            System.out.println(String.format("File '%s' does not exist", fileName));
-            return;
+            System.out.println(String.format("File '%s' does not exist", 
+                        fileName));
+            System.exit(0);
         }
 
-        try {
-            TimerTask task = new FileWatcher(file) {
-                protected void onChange(File file) {
-                    LocalDateTime now = LocalDateTime.now();
-                    System.out.println(String.format("%s <changed at %s>", 
-                        file.getName(),
-                        DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(now)));
+        TimerTask task = new FileWatcher(file) {
+            protected void onChange(File file) {
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println(String.format("%s <changed at %s>", 
+                    file.getName(),
+                    DateTimeFormatter.ofPattern(DateFormat).format(now)));
+                try {
+                    System.out.println(executeCommand(command));
                 }
-            };
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        };
 
-            Timer timer = new Timer();
-            timer.schedule(task, new Date(), pollRate);
-        } catch (IOException | NoSuchAlgorithmException ex) {
+        Timer timer = new Timer();
+        timer.schedule(task, new Date(), pollRate);
+    }
+
+    private static String executeCommand(String command) {
+        StringBuffer output = new StringBuffer();
+        String line = new String();
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            process.waitFor();
+            BufferedReader reader = 
+                new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
+            System.exit(1);
         }
 
-
+        return output.toString();
     }
 }
